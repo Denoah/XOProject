97bce3e55c3999b4a9f2a21819c37f0eeecc217b
el <na131@mail.ru>
2015-11-09 15:25:57 +0300

    Field - refactoring(rules part has been removed)

--- src/el/denoah/model/Field.java
+++ src/el/denoah/model/Field.java
@@ -1,10 +1,15 @@
 package el.denoah.model;
 
+import el.denoah.model.exceptions.InvalidPointException;
+import el.denoah.model.exceptions.PointAlreadyOccupiedException;
+
 import java.awt.*;
 
 public class Field {
 
     public static final int FIELD_SIZE = 3;
+    public static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -12,11 +17,25 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointException{
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException, PointAlreadyOccupiedException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
